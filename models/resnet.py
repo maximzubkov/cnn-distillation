c6cc318d1b39cdbe789a614891005f371addf4ab
maximzubkov <zubkov.md@phystech.edu>
2020-09-19 22:57:41 +0300

    Unfreeze fully connected layer

--- models/resnet.py
+++ models/resnet.py
@@ -13,15 +13,17 @@ class ResNet50(nn.Module):
         self.encoder = models.resnet50(pretrained=config.pretrained)
         if config.freeze_encoder:
             for name, child in self.encoder.named_children():
-                for param in child.parameters():
-                    param.requires_grad = False
+                if name != 'fc':
+                    for param in child.parameters():
+                        param.requires_grad = False
 
         self.classifier = nn.Linear(1000, 10)
         if config.is_teacher:
             childern = chain(self.classifier.named_children(), self.encoder.named_children())
             for name, child in childern:
-                for param in child.parameters():
-                    param.requires_grad = False
+                if name != 'fc':
+                    for param in child.parameters():
+                        param.requires_grad = False
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
         return self.classifier(self.encoder(image))
