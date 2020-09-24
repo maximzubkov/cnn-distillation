5b719fbbfa5a2301c1aa23a053e5bea081a1ace2
maximzubkov <zubkov.md@phystech.edu>
2020-09-25 01:20:46 +0300

    Optimized imports

--- configs/loss_config.py
+++ configs/loss_config.py
@@ -1,4 +1,3 @@
-from typing import Callable
 from dataclasses import dataclass
 
 
--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from .distillation_cifar_model import DistillationCifarModel  # noqa
+from .logits_discriminator_model import LogitsDiscriminatorCifarModel  # noqa
 from .resnet import ResNet50, ResNet18  # noqa
 from .single_cifar_model import SingleCifarModel  # noqa
-from .logits_discriminator_model import LogitsDiscriminatorCifarModel # noqa
